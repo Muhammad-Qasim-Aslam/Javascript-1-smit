7f8e8d0de855d0d1d6f76233039adaf38fd69b70
Muhammad-Qasim-Aslam <141229090+Muhammad-Qasim-Aslam@users.noreply.github.com>
2023-10-17 12:29:21 +0500

    Javascript Assignment-1 SMIT

---
+++ app.js
@@ -0,0 +1,139 @@
+// Alert-1
+
+// alert("Welcome to the Website!");
+
+// Alert-2
+
+// alert("Error! Please enter a valid password.");
+
+// Alert-3
+
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+// Alert-4
+
+// alert("Welcome to JS Land...");
+
+// alert("Happy Coding!");
+
+// Alert-5
+
+// console.log("Hello... I can run JS through my web browser's console.");
+
+// Variable-1
+
+// var username;
+
+// Variable-2
+
+// var myName = "Syed Muhammad Qasim Bukhari";
+
+// Variable-3
+
+// var message = "Hello World";
+
+// alert(message);
+
+// Variable-4
+
+// var studentName = "Qasim Bukhari";
+
+// var studentAge = 20;
+
+// var studentSkill = "Web & App Development";
+
+// alert(studentName);
+
+// alert(studentAge);
+
+// alert(studentSkill);
+
+// Variable-5
+
+// var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+
+// alert(pizza);
+
+// Variable-6
+
+// var email = "example@example.com";
+
+// alert("My email address is" + " " + email);
+
+// Variable-7
+
+// var book = "A smarter way to learn Javascript";
+
+// alert("I am trying learn from the book," + " " + book);
+
+// Variable-8
+
+// document.write("Yah! I can write HTML content through Javascript.");
+
+// Variable-9
+
+// var string = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+// document.write(string);
+
+// alert(string);
+
+// VARIABLES_FOR_NUMBERS-1
+
+// var age = 20;
+
+// alert("I am " + age + " years old.");
+
+// VARIABLES_FOR_NUMBERS-2
+
+// var n = 7;
+
+// alert("You have visited this site " + n + " times");
+
+// VARIABLES_FOR_NUMBERS-3
+
+// var birthYear = 2003;
+
+// document.write("My birth year is, " + birthYear);
+
+// VARIABLES_FOR_NUMBERS-4
+
+// var storeName = "XYZ Clothing Store";
+
+// var customerName = "John Doe";
+
+// var productName = "T-shirt";
+
+// var quantity = 5;
+
+// document.write(customerName + " ordered " + quantity + " " + productName + "(s) on " + storeName);
+
+// VARIABLE_NAMES-1
+
+// var name, age, address;
+
+// VARIABLE_NAMES-2
+
+// legal names
+
+// var myVariable;
+
+// var _internalVar;
+
+// var $price;
+
+// var counter123;
+
+// var first_Name;
+
+// illegal names
+
+// var 123abc;
+
+// var my-variable;
+
+// var @specialVar;
+
+// var var;
+
+// var my variable;
